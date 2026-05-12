c5507e6585463ad4eb625262a39e800192defa3c
Jaroslaw Krefft <tech@krefft.pl>
2026-05-12 09:59:49 +0200

    Dodanie komentarzy objaśniajacych dla kodu - rest_api_flask.py

--- week1/pyton_api/rest_api_flask.py
+++ week1/pyton_api/rest_api_flask.py
@@ -4,27 +4,56 @@
 # Konfiguracja REST-owego web service, wykorzystanie biblioteki Flask
 # Użycie metod GET i POST
 # Zatrzymanie, wyjście z serwera CTRL+C
+# Uwaga! - Do testów deweloperskich i na lokalnyn PC świadomie użyłem opcji debug=True znając zagrożenia za sobą niosące  .. poniżej wskazane.
+#                                                                                                                             Flask pokazuje:
+#                                                                                                                             traceback,
+#                                                                                                                             linie kodu,
+#                                                                                                                             nazwy plików,
+#                                                                                                                             ścieżki systemowe,
+#                                                                                                                             zmienne,
+#                                                                                                                             fragmenty kodu.
 
-# dodanie opcji Debug włącza: debugger,
-# automatyczny restart, szczegółowe błędy.
 
 from flask import Flask, jsonify, request
 
-
+# stworzenie instancji aplikacji Flask
 app = Flask(__name__)
 
 
-@app.route('/test', methods=['GET']) #endpoint GET
+
+# ENDPOINT GET
+# endpoint testowy 
+# http://127.0.0.1:3000/test
+# ten endpoint odpowiada na zapytania GET
+
+@app.route('/test', methods=['GET']) 
 def test():
-    return jsonify(result='to jest test')
+    # jsonify zmienia dane Python do formatu JSON
+    return jsonify(result='to jest test',
+                   status='success')
 
+# ENDPONT POST - odbierajacy JSON
+# methods = ['POST'] - endpoint odbiera dane wysłane metoda POST
 
-@app.route('/test' , methods=['POST']) #endpoin 2
+@app.route('/test' , methods=['POST']) 
 def test_post():
+
+    # pobranie danych JSON z requesta
+    # request = obiekt Flask reprezentujący zapytanie HTTP
     data = request.get_json()
 
+    # walidacja - jeśli klient nie wyśle JSON otrzymamy komunikat o błędzie 400
     if not data:
         return jsonify(error="Brak JSON"), 400
     return jsonify(data), 201
 
-app.run(port=3000, debug = True)
+# START APLIKACJI
+# __name__ == "__main__":  -   warunek uruchomienia aplikacji przy bezpośrednim uruchomieniu pliku. 
+
+# debug=True:
+# - automatyczny restart po zmianie kodu
+# - dokładne błędy Flask
+# - tryb developerski
+
+if __name__ == "__main__":
+    app.run(port=3000, debug = True)
